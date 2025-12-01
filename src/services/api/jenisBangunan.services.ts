541590b78e157100b3f8bbb9cc29f6419c21de75
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-12-02 05:58:48 +0700

    change endpoint fungsi bangunan

--- src/services/api/jenisBangunan.services.ts
+++ src/services/api/jenisBangunan.services.ts
@@ -2,7 +2,8 @@ import axiosInstance from "@/lib/axiosInstance";
 import endpoint from "./endpoint";
 
 const jenisBangunanServices = {
-  getJenisBangunan: () => axiosInstance.get(`${endpoint.MASTER}/fungsi/`),
+  getJenisBangunan: () =>
+    axiosInstance.get(`${endpoint.MASTER}/kategori-fungsi/`),
 };
 
 export default jenisBangunanServices;
